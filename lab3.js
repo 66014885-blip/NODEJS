7c062eafde9a3b7b13ddcc0cbf20ad9d5c274320
66014885-blip <66014885@rbac.ac.th>
2025-11-11 00:42:44 +0700

    commit1

--- lab3.js
+++ lab3.js
@@ -1,8 +1,8 @@
 let fname = "Donut";
 let lname = "Yuanyi";
 let age = 25;
-let height = 160; // in cm
-let weight = 42; // in kg   
+let height = 160000; // in cm
+let weight = 42000; // in g
 
 
 
